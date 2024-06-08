e0dba043fdf607e6ecea4415973d0638b01302c9
brn2bwild <brn_2_b_wild@hotmail.com>
2024-06-08 12:31:09 -0600

    Actualizados los permisos y los roles.

--- database/seeders/PermissionsSeeer.php
+++ database/seeders/PermissionsSeeer.php
@@ -17,28 +17,68 @@ class PermissionsSeeer extends Seeder
 		// Reset cached roles and permissions
 		app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-		// create permissions
+		// create resource permissions
+		Permission::create(['name' => 'ver recursos']);
+		Permission::create(['name' => 'crear recursos']);
+		Permission::create(['name' => 'editar recursos']);
+		Permission::create(['name' => 'eliminar recursos']);
+
+		// create users permissions
+		Permission::create(['name' => 'ver usuarios']);
+		Permission::create(['name' => 'editar usuarios']);
+		Permission::create(['name' => 'eliminar usuarios']);
+
+		// create publications permissions
 		Permission::create(['name' => 'ver publicaciones']);
+		Permission::create(['name' => 'crear publicaciones']);
 		Permission::create(['name' => 'editar publicaciones']);
 		Permission::create(['name' => 'eliminar publicaciones']);
-		Permission::create(['name' => 'crear publicaciones']);
+
+		// create admin-users permissions
+		Permission::create(['name' => 'ver administradores']);
+		Permission::create(['name' => 'crear administradores']);
+		Permission::create(['name' => 'editar administradores']);
+		Permission::create(['name' => 'eliminar administradores']);
 
 		// create roles and assign existing permissions
 		$userRole = Role::create(['name' => 'user']);
+		$userRole->givePermissionTo('ver recursos');
 		$userRole->givePermissionTo('ver publicaciones');
+		$userRole->givePermissionTo('crear publicaciones');
+		$userRole->givePermissionTo('editar publicaciones');
+		$userRole->givePermissionTo('eliminar publicaciones');
 
 		$editorRole = Role::create(['name' => 'editor']);
+		$editorRole->givePermissionTo('ver recursos');
+		$editorRole->givePermissionTo('crear recursos');
+		$editorRole->givePermissionTo('editar recursos');
+		$editorRole->givePermissionTo('eliminar recursos');
+		$editorRole->givePermissionTo('ver publicaciones');
+		$editorRole->givePermissionTo('crear publicaciones');
 		$editorRole->givePermissionTo('editar publicaciones');
+		$editorRole->givePermissionTo('eliminar publicaciones');
 
 		$adminRole = Role::create(['name' => 'admin']);
+		$adminRole->givePermissionTo('ver recursos');
+		$adminRole->givePermissionTo('crear recursos');
+		$adminRole->givePermissionTo('editar recursos');
+		$adminRole->givePermissionTo('eliminar recursos');
+		$adminRole->givePermissionTo('ver usuarios');
+		$adminRole->givePermissionTo('editar usuarios');
+		$adminRole->givePermissionTo('eliminar usuarios');
+		$adminRole->givePermissionTo('ver publicaciones');
+		$adminRole->givePermissionTo('crear publicaciones');
 		$adminRole->givePermissionTo('editar publicaciones');
 		$adminRole->givePermissionTo('eliminar publicaciones');
-		$adminRole->givePermissionTo('crear publicaciones');
+		$adminRole->givePermissionTo('ver administradores');
+		$adminRole->givePermissionTo('crear administradores');
+		$adminRole->givePermissionTo('editar administradores');
+		$adminRole->givePermissionTo('eliminar administradores');
 
-		$superAdminRole = Role::create(['name' => 'super admin']);
 		// gets all permissions via Gate::before rule; see AuthServiceProvider
+		$superAdminRole = Role::create(['name' => 'super admin']);
 
-		// create demo users
+		// create system users
 		$user = User::factory()->create([
 			'name' => 'Example User',
 			'email' => 'user@example.com',
