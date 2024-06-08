0e9da622c6dc0f5fd39493307cc62dfa720ba6e0
brn2bwild <brn_2_b_wild@hotmail.com>
2024-06-08 12:30:12 -0600

    Agregada la función para mostrar imágenes de perfil en controlador de los archivos.

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
-	public function show(Request $request)
+	public function showPublicFile(Request $request)
 	{
-		$content = Storage::get('public/files/'.$request->file);
+		$content = Storage::get('public/files/' . $request->file);
 
 		return Response::make($content, 200, [
 			'Content-Type' => 'application/pdf',
 			'Content-Disposition' => 'inline; filename="' . $request->file . '"'
+		]);
+	}
+
+	public function showProfileImage(Request $request)
+	{
+		$content = Storage::get('profile_images/'. $request->image);
+
+		$type = Storage::mimeType('profile_images/'. $request->image);
+
+		return Response::make($content, 200, [
+			'Content-Type' => $type,
 		]);
 	}
 }
