314081542cfd038afc0264eeea3d9617a4064849
brn2bwild <brn_2_b_wild@hotmail.com>
2024-06-12 08:31:19 -0600

    Agregados los componentes de los botones editar y eliminar del resourceCard.

---
+++ resources/js/Components/DeleteButton.vue
@@ -0,0 +1,8 @@
+<template>
+    <button class="items-center">
+        <font-awesome-icon
+            :icon="{ prefix: 'fa', iconName: 'trash' }"
+            class="rounded-full bg-red-700 p-2 text-xl text-neutral-50 transition-all duration-200 hover:bg-red-500"
+        />
+    </button>
+</template>
---
+++ resources/js/Components/EditButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="items-center">
+        <font-awesome-icon
+            :icon="{
+                prefix: 'fa',
+                iconName: 'pen-to-square',
+            }"
+            class="rounded-full bg-orange-700 p-2 text-xl text-neutral-50 transition-all duration-200 hover:bg-orange-500"
+        />
+    </button>
+</template>
