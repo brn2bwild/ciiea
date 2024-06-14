f38dbb29aebf281f6c5b7b9f8d2c59cd36d43824
brn2bwild <brn_2_b_wild@hotmail.com>
2024-06-14 15:59:45 -0600

    Cambiada la fuente del proyecto en general.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                sans: ["Roboto", ...defaultTheme.fontFamily.sans],
             },
         },
     },
